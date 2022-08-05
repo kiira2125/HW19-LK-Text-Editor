675a3ec392add7f70eaa5ec6a22c99ef1b6ad7aa
LK <lunakiira0911@gmail.com>
2022-08-04 19:39:20 -0700

    new updates

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ||3000;
 
-app.use(express.static('../client/dist'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static('client/dist'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()):
 
-require('./routes/htmlRoutes')(app);
+require('/routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
