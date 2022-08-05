b9960586992110049084c7fa56f901a8cdb183c2
LK <lunakiira0911@gmail.com>
2022-08-04 19:48:45 -0700

    updating the src-sw.js file

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,41 +1,18 @@
 const { warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { registerRoute} = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');  
+const { ExpirationPlugin} = require ('workbox-expiration');
+const { precacheAndRoute} = require('workbox-precaching/precacheAndRoute');
 
-precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(self._WB_MANIFEST);
 
-// Set up page cache
-const pageCache = new CacheFirst({
+// need to do a page cache
+const pageCache= new CacheFirst({
   cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
+  plugins:[
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 *60,
     }),
   ],
 });
-
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// Set up asset cache
-registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
-);
